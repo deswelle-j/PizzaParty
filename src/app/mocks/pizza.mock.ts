ffd4cc93f49fe3197a057e0d46696dff55272cd8
deswelle-j <deswelle.johan@gmail.com>
2018-02-08 12:06:31 +0100

    Edit package.json

--- src/app/mocks/pizza.mock.ts
+++ src/app/mocks/pizza.mock.ts
@@ -1,8 +1,8 @@
 import { Pizza } from '../models/pizza.model';
 
 export const PIZZAS : Pizza[] = [
-  { id: 1, name: 'Reine', price: 12, image: '../../../assets/img/1.jpg'},
-  { id: 2, name: '4 fromages', price: 13, image: '../../assets/img/2.jpg' },
-  { id: 3, name: 'Orientale', price: 11, image: '../../assets/img/3.jpg' },
-  { id: 4, name: 'Cannibale', price: 9, image: '../../assets/img/4.jpg' }
+  { id: 1, name: 'Reine', price: 12, image: 'assets/img/1.jpg'},
+  { id: 2, name: '4 fromages', price: 13, image: 'assets/img/2.jpg' },
+  { id: 3, name: 'Orientale', price: 11, image: 'assets/img/3.jpg' },
+  { id: 4, name: 'Cannibale', price: 9, image: 'assets/img/4.jpg' }
 ];
